c66034a3f3cc8e72a9e704963bcc5dc1724a0a10
AngelSilva98 <angelceesd@gmail.com>
2023-09-10 22:45:00 -0300

    creacion landing page

--- playclub/src/Views/Landing/Landing.jsx
+++ playclub/src/Views/Landing/Landing.jsx
@@ -4,11 +4,31 @@ export default function Landing() {
   return (
     <div className={style.container}>
       <div className={style.contentInfo}>
-        <h1 className={style.titulo}>Bienvenido a PlayClub </h1>
-        <a className={style.btn} href="/home">
-          Iniciar
-        </a>
+        <div className={style.containerBienvenido}>
+          <div className={style.contImage}>
+            <img
+              src="https://res.cloudinary.com/dou3yyisb/image/upload/v1694394442/btc-icon-arreglo_xzuvh2.png"
+              alt="bitcoin icon"
+              width="500"
+            />
+          </div>
+
+          <img
+            src="https://res.cloudinary.com/dou3yyisb/image/upload/v1694392276/PlayGame/Logo_f7tdmv.png"
+            alt="Logo playclub"
+          />
+          <div className={style.contImage}>
+            <img
+              src="https://res.cloudinary.com/dou3yyisb/image/upload/v1694395421/usdt-icon-arregladooooo_tqgoct.png"
+              alt="usdt icon"
+              width="350"
+            />
+          </div>
+        </div>
       </div>
+      <a className={style.btn} href="/home">
+        Iniciar
+      </a>
     </div>
   );
 }
