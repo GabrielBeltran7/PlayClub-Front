9fef52b3389a6b33ddc7f42d4c3cb756eacd1c98
AngelSilva98 <angelceesd@gmail.com>
2023-09-18 22:01:14 -0300

    Corredores y register

--- src/Components/AllCorredores/AllCorredores.jsx
+++ src/Components/AllCorredores/AllCorredores.jsx
@@ -1,18 +1,63 @@
 import { useDispatch, useSelector } from "react-redux";
 import React, { useRef, useState, useEffect } from "react";
 import { getCorredores } from "../../Redux/Actions";
+import { Table } from "antd";
+
 const AllCorredores = () => {
   const allCorredores = useSelector((state) => state.corredor);
   const dispatch = useDispatch();
-  console.log("all corredores", allCorredores);
   useEffect(() => {
     dispatch(getCorredores());
-  }, [allCorredores.length]);
+  }, [dispatch]);
+
+  const allCorredoresFilter = allCorredores.filter(
+    (corredor) => corredor.activo === true
+  );
+
+  console.log(allCorredoresFilter);
+  const columns = [
+    {
+      title: "Nombre corredor",
+      width: "1%",
+      dataIndex: "nombre",
+      key: "nombre",
+      fixed: "left",
+    },
+    {
+      title: "Numero corredor",
+      width: "1%",
+      dataIndex: "numero",
+      key: "numero",
+      sorter: true,
+      fixed: "left",
+    },
+    {
+      title: "Descripcion",
+      dataIndex: "descripcion",
+      key: "1",
+      width: "5%",
+    },
+    {
+      title: "Action",
+      key: "operation",
+      width: "1%",
+      fixed: "right",
+      render: (_, record) => (
+        <a onClick={() => handleSubmit(record)}>Deshabilitar Corredor</a>
+      ),
+    },
+  ];
+  const handleSubmit = (record) => {
+    console.log("recorddddd", record);
+  };
   return (
-    <div>
-      <h1>corredores</h1>
-    </div>
+    <Table
+      columns={columns}
+      dataSource={allCorredoresFilter}
+      scroll={{
+        x: 1300,
+      }}
+    />
   );
 };
-
 export default AllCorredores;
